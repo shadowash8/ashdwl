6877d243e38fc9346861adbfbeff0020f0fce9da
A S H <shadowash8@protonmail.com>
2026-03-05 11:17:03 +0530

    feat add waybar

--- autostart.sh
+++ autostart.sh
@@ -4,30 +4,14 @@
 # We don't use '&' because we need these files to exist before the next steps.
 wal -R
 
+rm ~/.config/waybar -r
+ln -s ~/.ashdwl/waybar/ ~/.config/waybar
+
 # Startup
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-waybar &
+dbus-launch waybar &
 dunst &
 swaybg -i $(cat ~/.cache/wal/wal) &
-
-# Handle the XWayland/XRDB mess in a delayed block.
-(
-    while true; do
-        # Check if xeyes (XWayland anchor) is running
-        if ! pgrep -x "xeyes" > /dev/null; then
-            # Re-anchor XWayland
-            xeyes -geometry 1x1-1-1 &
-            
-            sleep 1
-
-            # Re-merge the database
-            xrdb -merge ~/.Xresources
-        fi
-        
-        # Check every 30 seconds—low overhead, but fast recovery
-        sleep 30
-    done
-) &
 
 # Clipboard stuff
 cliphist wipe &
